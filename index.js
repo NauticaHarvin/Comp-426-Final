b464b1a634d179b39214a55d90f37ea54098e520
Nautica Harvin <ThisIsNautica@Outlook.Com>
2021-05-08 11:10:51 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const functions = require("firebase-functions");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
- exports.helloWorld = functions.https.onRequest((request, response) => {
-   response.send("Hello from Firebase!");
- });
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ theme.js
@@ -0,0 +1,15 @@
+// theme.js
+export const lightTheme = {
+    body: '#E2E2E2',
+    text: '#33C9FF',
+    toggleBorder: '#FFF',
+    gradient: 'linear-gradient(#39598A, #79D7ED)',
+    
+  }
+  
+  export const darkTheme = {
+    body: '#363537',
+    text: '#8800FF',
+    toggleBorder: '#6B8096',
+    gradient: 'linear-gradient(#091236, #1E215D)',
+  }
